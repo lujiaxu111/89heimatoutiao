ea5f086a6944868f529f35d05afe0548006d951c
lujiaxu111 <1843953361@qq.com>
2019-12-18 20:12:16 +0800

    主页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-      登陆页面
+  <div class="login">
+      <el-card class="login-card">
+          <div class="title">
+              <img src="../../assets/img/logo_index.png" alt="">
+          </div>
+          <el-form style="margin-top:30px">
+              <el-form-item>
+                  <el-input placeHolder="请输入手机号"></el-input>
+              </el-form-item>
+              <el-form-item>
+                  <el-input style="width:65%" placeHolder="请输入验证码"></el-input>
+                  <el-button style="float:right" plain>发送 验证码</el-button>
+              </el-form-item>
+              <el-form-item>
+                   <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+              </el-form-item>
+              <el-form-item>
+                  <el-button type="primary" style="width:100%">
+                      登录
+                  </el-button>
+              </el-form-item>
+          </el-form>
+          </el-card>
   </div>
 </template>
 
@@ -10,6 +31,23 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.login{
+    background-image: url("../../assets/img/login_bj.jpg");
+    background-size: cover;
+    height:100vh;
+    display: flex;
+     justify-content: center;
+     align-items: center;
+    .login-card{
+        width: 440px;
+        height: 350px;
+        .title{
+            text-align: center;
+            img{
+                height: 44px;
+            }
+        }
+    }
+}
 </style>
